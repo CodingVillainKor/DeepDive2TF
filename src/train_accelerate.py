52faff06d1c61084751af1cdda508377f7a3e258
hrim <commit.im@gmail.com>
2025-09-16 05:41:21 +0900

    add train_accelerate

---
+++ src/train_accelerate.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn.functional as F
+
+from transformer import Transformer
+from data import dl, tokenizer
+
+m = Transformer(
+    input_dim=tokenizer.vocab_size,
+    output_dim=tokenizer.vocab_size,
+    n_heads=8,
+    n_layers=8,
+    hidden_dim=512,
+    dropout=0.0,
+)
+
+optim = torch.optim.Adam(m.parameters(), lr=0.0001)
+for e in range(100):
+    for i, (question, answer) in enumerate(dl):
+        optim.zero_grad()
+        loss = m(question, answer)
+        loss.backward()
+        optim.step()
+        print(f"\r{e} / {100} | loss = {loss:.3f}", end="")
+
+# save model
+torch.save(m.state_dict(), "model.pth")
+
+# inference with training dataset sample
+m.eval()
+sample = next(iter(dl))[0]
+out = m.infer(sample)
+for o in out:
+    text = tokenizer.decode(o.tolist(), skip_special_tokens=True)
+    print(text)
+    print("-----")
+    break
